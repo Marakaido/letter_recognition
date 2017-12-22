391f05d080a9e6582e13c0db3165f11d2a8a3ae2
Marakaido <yaroslav.mykoliv28@gmail.com>
2017-12-22 04:43:31 +0200

    Generate networks

--- image_neuro_study.m
+++ image_neuro_study.m
@@ -1,4 +1,4 @@
-function net = image_neuro_study(saveFileName, goal)
+function [net,tr] = image_neuro_study(saveFileName, goal, times)
 [x,t] = train_data('train', '.bmp');
 net = newff(x, t, [16 12], {'logsig', 'logsig'});
 
@@ -7,18 +7,24 @@ net.performFcn = 'mse';
 net.trainParam.epochs=2000;
 net.trainParam.goal=goal;
 
-[net, tr] = train(net,x,t);
-y = sim(net, x);
-perf = perform(net,y,t);
-counter = 1;
-while perf > 0.1 && counter < 50
+[best_net, best_tr] = train(net,x,t);
+best_perf = perform(best_net,t,best_net(x));
+for i = 1:times
     net = init(net);
-    net = train(net, x, t);
-    y = net(x);
-    perf = perform(net, y, t)
-    counter = counter+1;
+    [net, tr] = train(net, x, t);
+    perf = perform(net, t, net(x));
+    if perf < best_perf
+        best_perf = perf;
+        best_net = net;
+        best_tr = tr;
+    end
 end
 
-figure, plotconfusion(t,y)
+figure, plotconfusion(t, best_net(x))
+view(best_net)
+
+net = best_net;
+tr = best_tr;
+time = sum(tr.time);
 
 save(saveFileName)
