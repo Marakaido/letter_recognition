6a1ae9d1f0d570c52ae4d641f8ab24ea305391dc
Marakaido <yaroslav.mykoliv28@gmail.com>
2017-12-22 03:12:07 +0200

    Automatically generate matrix of target classes in training data generation

--- train_data.m
+++ train_data.m
@@ -2,12 +2,13 @@ function [P, T] = train_data(folder, ext)
 
 group_file_table = readtable([folder '/file_name.txt']);
 
+vectors = [1 0 0 0
+           0 1 0 0
+           0 0 1 0
+           0 0 0 1];
+       
 for i = 1:height(group_file_table)
     t = im2double(imread(strcat(folder, '/', group_file_table{i,2}{1}, ext)));
     P(:, i) = t(:);
+    T(:, i) = vectors(:, mod(group_file_table{i,1}-1, 4)+1);
 end
-
-T = [1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0
-     0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0
-     0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0
-     0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1 0 0 0 1];
